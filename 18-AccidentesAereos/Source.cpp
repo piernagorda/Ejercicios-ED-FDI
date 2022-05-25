af297f47ee9c146b53486786105f02db58238e54
Javier Piernagorda <40919203+piernagorda@users.noreply.github.com>
2022-05-25 20:58:35 +0200

    Exercise #18 solved
    Solved exercise

---
+++ 18-AccidentesAereos/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <string>
+
+struct tDatos {
+	std::string fecha;
+	int muertes;
+};
+
+bool resuelveCaso() {
+	int n;
+	std::cin>>n;
+	if (!std::cin) return false;
+	std::stack<tDatos> colaAccidentes;
+	for (int i = 0; i < n; ++i) {
+		std::string temp;
+		int tempMuertes;
+		std::cin >> temp >> tempMuertes;
+		if (colaAccidentes.empty()) std::cout << "NO HAY \n";
+		else {
+			while (!colaAccidentes.empty() && tempMuertes >= colaAccidentes.top().muertes) colaAccidentes.pop();
+			if (colaAccidentes.empty()) std::cout << "NO HAY \n";
+			else std::cout << colaAccidentes.top().fecha << std::endl;
+		}
+		colaAccidentes.push({ temp, tempMuertes });
+	}
+	std::cout << "---\n";
+	return true;
+}
+
+int main() {
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif // !DOMJUDGE
+	while(resuelveCaso());
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif // !DOMJUDGE
+
+}
